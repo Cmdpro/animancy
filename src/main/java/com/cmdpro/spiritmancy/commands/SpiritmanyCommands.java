4a2db1ff430e3a6e0a7130aa4c4ba58ed7752444
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2023-11-30 21:24:15 -0500

    a

--- src/main/java/com/cmdpro/spiritmancy/commands/SpiritmanyCommands.java
+++ src/main/java/com/cmdpro/spiritmancy/commands/SpiritmanyCommands.java
@@ -4,13 +4,16 @@ import com.cmdpro.spiritmancy.Spiritmancy;
 import com.cmdpro.spiritmancy.api.SpiritmancyUtil;
 import com.cmdpro.spiritmancy.moddata.PlayerModData;
 import com.cmdpro.spiritmancy.moddata.PlayerModDataProvider;
+import com.klikli_dev.modonomicon.book.Book;
 import com.klikli_dev.modonomicon.bookstate.BookUnlockStateManager;
+import com.klikli_dev.modonomicon.data.BookDataManager;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 
@@ -37,6 +40,13 @@ public class SpiritmanyCommands {
                                 })
                         )
                 )
+                .then(Commands.literal("setancientknowledge")
+                        .then(Commands.argument("amount", IntegerArgumentType.integer(0))
+                                .executes((command) -> {
+                                    return setancientknowledge(command);
+                                })
+                        )
+                )
                 .then(Commands.literal("resetlearned")
                         .executes((command) -> {
                             return resetlearned(command);
@@ -49,6 +59,7 @@ public class SpiritmanyCommands {
             Player player = (Player) command.getSource().getEntity();
             player.getCapability(PlayerModDataProvider.PLAYER_MODDATA).ifPresent(data -> {
                 data.getUnlocked().clear();
+                BookUnlockStateManager.get().resetFor((ServerPlayer)player, BookDataManager.get().getBook(new ResourceLocation("spiritmancy", "spiritmancyguide")));
                 BookUnlockStateManager.get().updateAndSyncFor((ServerPlayer)player);
             });
         }
@@ -78,6 +89,16 @@ public class SpiritmanyCommands {
             player.getCapability(PlayerModDataProvider.PLAYER_MODDATA).ifPresent(data -> {
                 int knowledge = command.getArgument("amount", int.class);
                 data.setKnowledge(knowledge);
+            });
+        }
+        return Command.SINGLE_SUCCESS;
+    }
+    private static int setancientknowledge(CommandContext<CommandSourceStack> command){
+        if(command.getSource().getEntity() instanceof Player) {
+            Player player = (Player) command.getSource().getEntity();
+            player.getCapability(PlayerModDataProvider.PLAYER_MODDATA).ifPresent(data -> {
+                int knowledge = command.getArgument("amount", int.class);
+                data.setAncientKnowledge(knowledge);
             });
         }
         return Command.SINGLE_SUCCESS;
