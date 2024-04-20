232815b2dd6073ad6f9b43723198bb3eb7ce08eb
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2024-04-19 22:11:06 -0400

    a

--- src/main/java/com/cmdpro/spiritmancy/integration/BookAltarRecipePage.java
+++ src/main/java/com/cmdpro/spiritmancy/integration/BookAltarRecipePage.java
@@ -4,6 +4,8 @@ import com.cmdpro.spiritmancy.recipe.SoulAltarRecipe;
 import com.google.gson.JsonObject;
 import com.klikli_dev.modonomicon.api.ModonomiconConstants.Data.Page;
 import com.klikli_dev.modonomicon.book.BookTextHolder;
+import com.klikli_dev.modonomicon.book.conditions.BookCondition;
+import com.klikli_dev.modonomicon.book.conditions.BookNoneCondition;
 import com.klikli_dev.modonomicon.book.page.BookRecipePage;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
@@ -14,20 +16,24 @@ import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
 
 public class BookAltarRecipePage extends BookRecipePage<Recipe<?>> {
-    public BookAltarRecipePage(BookTextHolder title1, ResourceLocation recipeId1, BookTextHolder title2, ResourceLocation recipeId2, BookTextHolder text, String anchor) {
-        super(SoulAltarRecipe.Type.INSTANCE, title1, recipeId1, title2, recipeId2, text, anchor);
+    public BookAltarRecipePage(BookTextHolder title1, ResourceLocation recipeId1, BookTextHolder title2, ResourceLocation recipeId2, BookTextHolder text, String anchor, BookCondition condition) {
+        super(SoulAltarRecipe.Type.INSTANCE, title1, recipeId1, title2, recipeId2, text, anchor, condition);
     }
 
     public static BookAltarRecipePage fromJson(JsonObject json) {
         var common = BookRecipePage.commonFromJson(json);
         var anchor = GsonHelper.getAsString(json, "anchor", "");
-        return new BookAltarRecipePage(common.title1(), common.recipeId1(), common.title2(), common.recipeId2(), common.text(), anchor);
+        var condition = json.has("condition")
+                ? BookCondition.fromJson(json.getAsJsonObject("condition"))
+                : new BookNoneCondition();
+        return new BookAltarRecipePage(common.title1(), common.recipeId1(), common.title2(), common.recipeId2(), common.text(), anchor, condition);
     }
 
     public static BookAltarRecipePage fromNetwork(FriendlyByteBuf buffer) {
         var common = BookRecipePage.commonFromNetwork(buffer);
         var anchor = buffer.readUtf();
-        return new BookAltarRecipePage(common.title1(), common.recipeId1(), common.title2(), common.recipeId2(), common.text(), anchor);
+        var condition = BookCondition.fromNetwork(buffer);
+        return new BookAltarRecipePage(common.title1(), common.recipeId1(), common.title2(), common.recipeId2(), common.text(), anchor, condition);
     }
 
     @Override
@@ -37,12 +43,6 @@ public class BookAltarRecipePage extends BookRecipePage<Recipe<?>> {
         }
 
         return recipe.getResultItem(level.registryAccess());
-    }
-
-    @Override
-    public void toNetwork(FriendlyByteBuf buffer) {
-        super.toNetwork(buffer);
-        buffer.writeUtf(this.anchor);
     }
 
     @Override
